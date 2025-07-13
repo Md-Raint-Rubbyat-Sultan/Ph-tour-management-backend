32ef52453296b729783721d93822b25149d1145e
Md-Raint-Rubbyat-Sultan <mdranitrubbyatsultan@gmail.com>
2025-07-14 01:04:42 +0600

    setup passport to apply google authentication

---
+++ src/app/config/passport.ts
@@ -0,0 +1,51 @@
+import passport from "passport";
+import {
+  Strategy as googleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
+import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: envVars.GOOGLE_CLIENT_ID,
+      clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+      callbackURL: envVars.GOOGLE_CALLBACK_URL,
+    },
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const email = profile.emails?.[0].value;
+
+        if (!email) {
+          return done(null, false, { message: "Email not found." });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            isVarified: true,
+            auth: [{ provider: "google", providerId: profile.id }],
+            role: Role.USER,
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        return done(error);
+      }
+    }
+  )
+);
